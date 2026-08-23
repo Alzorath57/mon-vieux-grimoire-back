9a348080cd08ba4fdfe71523ea2b37db314048a1
Alzorath57 <julien.holtzinger@gmail.com>
2026-08-23 21:40:12 +0200

    feat: connexion à MongoDB avec Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connecté à MongoDB"))
+  .catch((err) => console.error("Erreur de connexion à MongoDB :", err));
+
 app.use(express.json());
+
 app.use("/api/books", require("./routes/book"));
 app.use("/api/auth", require("./routes/auth"));
 
