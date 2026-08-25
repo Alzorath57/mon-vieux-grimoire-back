37013bc7ea4860117e381a303994ebbb6f35a45b
Alzorath57 <julien.holtzinger@gmail.com>
2026-08-25 20:49:34 +0200

    feat: création d'un livre avec upload d'image (POST /api/books)

--- controllers/book.js
+++ controllers/book.js
@@ -9,3 +9,18 @@ exports.getAllBooks = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// méthode Post pour création d'un nouveau livre
+exports.createBook = async (req, res) => {
+  const bookObject = JSON.parse(req.body.book);
+  const book = new Book({
+    ...bookObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+  });
+  try {
+    await book.save();
+    res.status(201).json({ message: "Livre créé avec succès !" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/book");
+const multer = require("../middleware/multer-config");
 
 router.get("/", bookController.getAllBooks);
+router.post("/", multer, bookController.createBook);
 
 module.exports = router;
