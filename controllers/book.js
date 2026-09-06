b1569054b234a7892be67322f85c9f59146f9f51
Alzorath57 <julien.holtzinger@gmail.com>
2026-09-06 12:06:28 +0200

    fix: sécurisation des try/catch pour englober toute la logique métier

--- controllers/book.js
+++ controllers/book.js
@@ -40,14 +40,14 @@ exports.getBook = async (req, res) => {
  * @param {import('express').Response} res - La réponse Express.
  */
 exports.createBook = async (req, res) => {
-  const filename = await optimizeImage(req.file);
-  const bookObject = JSON.parse(req.body.book);
-  const book = new Book({
-    ...bookObject,
-    userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${filename}`,
-  });
   try {
+    const filename = await optimizeImage(req.file);
+    const bookObject = JSON.parse(req.body.book);
+    const book = new Book({
+      ...bookObject,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get("host")}/images/${filename}`,
+    });
     await book.save();
     res.status(201).json({ message: "Livre créé avec succès !" });
   } catch (err) {
@@ -62,32 +62,31 @@ exports.createBook = async (req, res) => {
  * @param {import('express').Response} res - La réponse Express.
  */
 exports.updateBook = async (req, res) => {
-  const oldBook = await Book.findById(req.params.id);
-  if (!oldBook) {
-    return res.status(404).json({ error: "Livre introuvable" });
-  }
-  if (oldBook.userId !== req.auth.userId) {
-    return res.status(403).json({ error: "unauthorized request" });
-  }
   let bookObject;
-  
-  if (req.file) {
-    const filename = await optimizeImage(req.file);
-    const parts = oldBook.imageUrl.split("/images/");
-    const oldfilename = parts[parts.length - 1];
-    fs.unlink(`images/${oldfilename}`, (err) => {
-      if (err) {
-        console.error("Erreur lors de la suppression de l'image :", err);
-      }
-    });
-    bookObject = {
-      ...(req.body.book ? JSON.parse(req.body.book) : oldBook.toObject()),
-      imageUrl: `${req.protocol}://${req.get("host")}/images/${filename}`,
-    };
-  } else {
-    bookObject = { ...req.body };
-  }
   try {
+    const oldBook = await Book.findById(req.params.id);
+    if (!oldBook) {
+      return res.status(404).json({ error: "Livre introuvable" });
+    }
+    if (oldBook.userId !== req.auth.userId) {
+      return res.status(403).json({ error: "unauthorized request" });
+    }
+    if (req.file) {
+      const filename = await optimizeImage(req.file);
+      const parts = oldBook.imageUrl.split("/images/");
+      const oldfilename = parts[parts.length - 1];
+      fs.unlink(`images/${oldfilename}`, (err) => {
+        if (err) {
+          console.error("Erreur lors de la suppression de l'image :", err);
+        }
+      });
+      bookObject = {
+        ...(req.body.book ? JSON.parse(req.body.book) : oldBook.toObject()),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${filename}`,
+      };
+    } else {
+      bookObject = { ...req.body };
+    }
     const updatedBook = await Book.findByIdAndUpdate(
       req.params.id,
       bookObject,
@@ -109,14 +108,14 @@ exports.updateBook = async (req, res) => {
  * @param {import('express').Response} res - La réponse Express.
  */
 exports.deleteBook = async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  if (!book) {
-    return res.status(404).json({ error: "Livre introuvable" });
-  }
-  if (book.userId !== req.auth.userId) {
-    return res.status(403).json({ error: "unauthorized request" });
-  }
   try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ error: "Livre introuvable" });
+    }
+    if (book.userId !== req.auth.userId) {
+      return res.status(403).json({ error: "unauthorized request" });
+    }
     const parts = book.imageUrl.split("/images/");
     const filename = parts[parts.length - 1];
     const deletedBook = await Book.findByIdAndDelete(req.params.id);
@@ -138,27 +137,27 @@ exports.deleteBook = async (req, res) => {
  * @param {import('express').Response} res - La réponse Express.
  */
 exports.rateBook = async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  if (!book) {
-    return res.status(404).json({ error: "Livre introuvable" });
-  }
-  const existingRating = book.ratings.find(
-    (rating) => rating.userId === req.auth.userId,
-  );
-  if (existingRating) {
-    return res.status(400).json({ error: "Tu as déja noté ce livre" });
-  }
-  if (req.body.rating > 5 || req.body.rating < 0) {
-    return res.status(400).json({ error: "La note doit être entre 0 et 5" });
-  }
-  book.ratings.push({
-    userId: req.auth.userId,
-    grade: req.body.rating,
-  });
-  const sum = book.ratings.reduce((total, rating) => total + rating.grade, 0);
-  const averageRating = sum / book.ratings.length;
-  book.averageRating = averageRating;
   try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ error: "Livre introuvable" });
+    }
+    const existingRating = book.ratings.find(
+      (rating) => rating.userId === req.auth.userId,
+    );
+    if (existingRating) {
+      return res.status(400).json({ error: "Tu as déja noté ce livre" });
+    }
+    if (req.body.rating > 5 || req.body.rating < 0) {
+      return res.status(400).json({ error: "La note doit être entre 0 et 5" });
+    }
+    book.ratings.push({
+      userId: req.auth.userId,
+      grade: req.body.rating,
+    });
+    const sum = book.ratings.reduce((total, rating) => total + rating.grade, 0);
+    const averageRating = sum / book.ratings.length;
+    book.averageRating = averageRating;
     await book.save();
     res.status(200).json(book);
   } catch (err) {
