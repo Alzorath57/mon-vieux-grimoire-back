8ed6cb206e48be014ee47b39338ee3d491a4e618
Alzorath57 <julien.holtzinger@gmail.com>
2026-09-09 14:49:46 +0200

    feat: activation de CORS et exposition du dossier images en fichiers statiques

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connecté à MongoDB"))
   .catch((err) => console.error("Erreur de connexion à MongoDB :", err));
 
+app.use(cors());
 app.use(express.json());
+app.use("/images", express.static("images"));
 
 app.use("/api/books", require("./routes/book"));
 app.use("/api/auth", require("./routes/auth"));
